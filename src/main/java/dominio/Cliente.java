f687fdfd6d007b996ee206eb2fa789592aecd842
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-08-24 20:13:41 -0500

    Se agregan los atributos privados, constructores, getters and setters y un metodo toString a la clase Cliente

--- src/main/java/dominio/Cliente.java
+++ src/main/java/dominio/Cliente.java
@@ -10,5 +10,88 @@ package dominio;
  * @author giovanni
  */
 public class Cliente {
-    
+
+    private int idCliete;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+    private double saldo;
+
+    public Cliente() {
+    }
+
+    public Cliente(int idCliete) {
+        this.idCliete = idCliete;
+    }
+
+    public Cliente(String nombre, String apellido, String telefono, double saldo) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.saldo = saldo;
+    }
+
+    public Cliente(int idCliete, String nombre, String apellido, String email, String telefono, double saldo) {
+        this.idCliete = idCliete;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.telefono = telefono;
+        this.saldo = saldo;
+    }
+
+    public int getIdCliete() {
+        return idCliete;
+    }
+
+    public void setIdCliete(int idCliete) {
+        this.idCliete = idCliete;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "idCliete=" + idCliete + ", nombre=" + nombre + ", apellido=" + apellido + ", email=" + email + ", telefono=" + telefono + ", saldo=" + saldo + '}';
+    }
+
 }
