9e4632a05b9a080f6cb8fbaecbbdd32fa0482713
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-08-25 14:03:05 -0500

    Se creo el metodo encontrar y el metodo insertar en el ClienteDao

--- src/main/java/datos/ClienteDaoJDBC.java
+++ src/main/java/datos/ClienteDaoJDBC.java
@@ -37,7 +37,7 @@ public class ClienteDaoJDBC {
         Connection conn = null;
         PreparedStatement stmt = null;
         ResultSet rs = null;
-        
+
         Cliente cliente = null;
         List<Cliente> clientes = new ArrayList<>();
 
@@ -45,26 +45,84 @@ public class ClienteDaoJDBC {
             conn = Conexion.getConnection();
             stmt = conn.prepareStatement(SQL_SELECT);
             rs = stmt.executeQuery();
-            
-            while(rs.next()) {
+
+            while (rs.next()) {
                 int idCliente = rs.getInt("id_cliente");
                 String nombre = rs.getString("nombre");
                 String apellido = rs.getString("apellido");
                 String email = rs.getString("email");
                 String telefono = rs.getString("telefono");
                 double saldo = rs.getDouble("saldo");
-                
+
                 cliente = new Cliente(idCliente, nombre, apellido, email, telefono, saldo);
                 clientes.add(cliente);
             }
         } catch (SQLException ex) {
             ex.printStackTrace(System.out);
-        }
-        finally {
+        } finally {
             Conexion.close(rs);
             Conexion.close(stmt);
             Conexion.close(conn);
         }
         return clientes;
+    }
+
+    public Cliente encontrar(Cliente cliente) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = Conexion.getConnection();
+            stmt = conn.prepareStatement(SQL_SELECT_BY_ID);
+            stmt.setInt(1, cliente.getIdCliete());
+            
+            rs = stmt.executeQuery();
+            rs.absolute(1); // Nos posicionamos al primer registro devuelto
+
+            String nombre = rs.getString("nombre");
+            String apellido = rs.getString("apellido");
+            String email = rs.getString("email");
+            String telefono = rs.getString("telefono");
+            double saldo = rs.getDouble("saldo");
+            
+            cliente.setNombre(nombre);
+            cliente.setApellido(apellido);
+            cliente.setEmail(email);
+            cliente.setTelefono(telefono);
+            cliente.setSaldo(saldo);
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            Conexion.close(rs);
+            Conexion.close(stmt);
+            Conexion.close(conn);
+        }
+        return cliente;
+    }
+    
+    public int insertar(Cliente cliente) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int rows = 0;
+
+        try {
+            conn = Conexion.getConnection();
+            stmt = conn.prepareStatement(SQL_INSERT);
+            
+            stmt.setString(1, cliente.getNombre());
+            stmt.setString(2, cliente.getApellido());
+            stmt.setString(3, cliente.getEmail());
+            stmt.setString(4, cliente.getTelefono());
+            stmt.setDouble(5, cliente.getSaldo());
+            
+            rows = stmt.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            Conexion.close(stmt);
+            Conexion.close(conn);
+        }
+        return rows;
     }
 }
